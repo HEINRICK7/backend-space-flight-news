5ebfa8979fb74b64a5a700d1d8eb27006e8c49dd
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-14 13:07:07 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
+
+const router = require('./routes.js');
+
+require('./database/index')
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+app.use(router);
+
+const port = process.env.PORT || 3333;
+
+
+app.listen( port, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+         console.log(`connected server http://localhost:${port}`);
+    }     
+})
+    
+   
