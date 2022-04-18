ab3a57c01ff1a8fb21538832480e87ffdbd55cf2
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-18 16:57:49 -0300

    create src/models/News.js

---
+++ src/models/News.js
@@ -0,0 +1,47 @@
+const mongoose = require('../database')
+
+const ResultSchema = new mongoose.Schema({
+     
+    id:{
+        type: Number,
+        require: true,
+    },
+    featured: {
+        type: Boolean,
+        require: true,
+    },
+    title: {
+        type: String,
+        require: true,
+    },
+    url: {
+        type: String,
+        require: true,
+    },
+    imageUrl: {
+        type: String,
+        require: true,
+    },
+    newsSite: {
+        type: String,
+        require: true,
+    },
+    summary: {
+        type: String,
+        require: true,
+    },
+    publishedAt:{
+        type: Date,
+    },
+    launches: {
+        type: Array,
+    },
+      
+    events:{
+        type: Array,
+    }
+});
+ResultSchema.index({title: 'text'});
+const Result= mongoose.model('Result', ResultSchema);
+
+module.exports = Result
