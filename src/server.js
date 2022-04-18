df5459aebf2f9377e91b309c051e6675bf0f9716
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-18 16:59:25 -0300

    modifield server.js

--- src/server.js
+++ src/server.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
-const router = require('./routes.js');
+const router = require('./routes.js')
+require( './database/index')
 
-require('./database/index')
+const script = require('./services/getSpaceFlightNews.js')
+const cronJob = require('./services/cronServices.js')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+cronJob.start()
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(morgan('dev'))
-app.use(router);
+app.use(router)
+script()
 
-const port = process.env.PORT || 3333;
-
+const port = process.env.PORT || 3333
 
 app.listen( port, (err)=>{
     if(err){
-        console.log(err);
+        console.log(err)
     }else{
-         console.log(`connected server http://localhost:${port}`);
+        console.log(`connected server http://localhost:${port}`)
     }     
 })
-    
-   
+
