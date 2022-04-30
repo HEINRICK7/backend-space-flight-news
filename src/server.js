cce33cddba7aea391ebcd945f69c407dc45961a7
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-30 07:56:02 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocs = require('./swagger.json')
 
 const router = require('./routes.js')
 require( './database/index')
@@ -16,6 +18,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 script()
 
 const port = process.env.PORT || 3333
